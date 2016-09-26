d3bb1b850fb034a58f9250bc121b3d2ae0b42880
Jevgeni Sultanov <jevgeni.sultanov@byroomaailm.ee>
2016-09-26 10:41:42 +0300

    Error page 404 header added

--- controller/error_controller.php
+++ controller/error_controller.php
@@ -14,6 +14,7 @@ class Error_Controller extends Page_Controller
     
     public function setErrorPageProperties(string $language, string $body, array $parameters): void
     {
+        header("HTTP/1.0 404 Not Found");
         $this->setPageLanguage($language);
         $this->setPageBody($body);
         $this->setPageParameters($parameters);
